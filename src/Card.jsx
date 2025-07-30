3f6f07b0e7613d86eff7a30d1adc301f33045bcd
Migael Pieterse <migael.pieterse@vcontractor.co.za>
2025-07-30 11:28:24 +0200

    card message

--- src/Card.jsx
+++ src/Card.jsx
@@ -10,11 +10,13 @@ export default () => (
       </div>
     </div>
     <div className="cardInside">
-      <h3 className="back">HAPPY BIRTHDAY vodoo!</h3>
-      <p>Dear Friend,</p>
+      <h3 className="back">HAPPY BIRTHDAY MY LOVE</h3>
+      <p>Dear Marcelle,</p>
       <p>
-        Happy birthday!! I hope your day is filled with lots of love and
-        laughter! May all of your birthday wishes come true.
+        Ek hoop jy het n amazing dag vol liefde, vreugde en geluk. Jy verdien
+        dit om bederf te word en om jou spesiale dag te geniet. Jy beteken meer
+        as die wereld vir my, en ek is so dankbaar om jou in my lewe te
+        hê. I love and cherish you so much!
       </p>
       <p className="name">Pawan</p>
     </div>
